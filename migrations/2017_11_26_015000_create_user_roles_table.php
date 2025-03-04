4be82e095e273b94f2a65eb86dc735782265413f
Nick Kharadze <vscodeart@gmail.com>
2025-03-05 01:56:15 +0400

    remove getDoctrineColumn method from migration file

--- migrations/2017_11_26_015000_create_user_roles_table.php
+++ migrations/2017_11_26_015000_create_user_roles_table.php
@@ -14,13 +14,14 @@ class CreateUserRolesTable extends Migration
     public function up()
     {
         Schema::create('user_roles', function (Blueprint $table) {
-            $type = DB::connection()->getDoctrineColumn(DB::getTablePrefix().'users', 'id')->getType()->getName();
-            if ($type == 'bigint') {
-                $table->bigInteger('user_id')->unsigned()->index();
-            } else {
-                $table->integer('user_id')->unsigned()->index();
-            }
-
+//            todo need to solve this issue
+//            $type = DB::connection()->getDoctrineColumn(DB::getTablePrefix().'users', 'id')->getType()->getName();
+//            if ($type == 'bigint') {
+//                $table->bigInteger('user_id')->unsigned()->index();
+//            } else {
+//                $table->integer('user_id')->unsigned()->index();
+//            }
+            $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->bigInteger('role_id')->unsigned()->index();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
